bd5b105b8f42f96d2c6e57b30094f597c9771606
Abdelrahman Gamal <62604408+abdogamal90@users.noreply.github.com>
2022-07-27 01:21:58 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,3 +1,13 @@
+const btn1 = document.querySelector('.btn-1')
+const btn2 = document.querySelector('.btn-2')
+const btn3 = document.querySelector('.btn-3')
+const main = document.querySelector('.main')
+const result = document.querySelector('.result')
+const winner = document.querySelector('.winner')
+const youScore = document.querySelector('.you')
+const comScore = document.querySelector('.com')
+
+
 
 
 let pcScore = 0;
@@ -9,6 +19,13 @@ var computerPlay = function () {
 }
 
 function gameplay(computerSelection, playerSelection) {
+    if (pcScore == 5) {
+        return "computer is winner"
+    }
+    else if (playerScore == 5) {
+        return "you are winner"
+    }
+    
 
     if (computerSelection === playerSelection) {
         return "Draw"
@@ -43,42 +60,32 @@ function gameplay(computerSelection, playerSelection) {
         return "Draw"
     }
     else {
+        
         pcScore++;
         return "computer has won"
     }
+    
 
 
 
 }
-function game() {
-    for (let i = 0; i < 5; i++) {
-        var playerSelection = prompt("Enter Paper, Rock Or Scissors");
+function game(playerSelection) {
         playerSelection = playerSelection.toUpperCase()
         const computerSelection = computerPlay()
-        console.log(gameplay(computerSelection, playerSelection))
-        console.log(`playerScore is : ${playerScore} ::::: computerScore is : ${pcScore}`)
-
-        
-
-    }
-    if (playerScore > pcScore) {
-        console.log("You are the winner")
-
-    }
-    else if (pcScore > playerScore) {
-        console.log("Computer is the winner")
-    }
-    else {
-        console.log("Draw")
-    }
+      
+        //result.textContent += `playerScore is : ${playerScore} ::::: computerScore is : ${pcScore}`
    
-    
-
-    
-
+        result.textContent = `${gameplay(computerSelection, playerSelection)}`
+        youScore.textContent = `${playerScore}`
+        comScore.textContent = `${pcScore}`
 }
-console.log(game())
 
-
-
-
+btn1.addEventListener('click', (e) => {
+    game(e.target.outerText)
+})
+btn2.addEventListener('click', (e) => {
+    game(e.target.outerText)
+})
+btn3.addEventListener('click', (e) => {
+    game(e.target.outerText)
+})
